16c260775abc331066c3258a6aefc7968430d21f
anupamsharma <anu.lnmiit.1985@gmail.com>
2013-11-28 11:20:08 +0530

    Update algo_util.py
    Adding method to return prime number till number n (including).
    It usage method known as Sieve of Eratosthenes.

--- algo_util.py
+++ algo_util.py
@@ -1,5 +1,26 @@
+import math
+import sys
+
 def get_primes(n):
+    """
+    Returns a n+1 length boolean list a indicating if i is prime by value of a[i]. It usage Sieve of Eratosthenes for
+    this.
+
+    """
     assert(n>1)
-    primes = [True for i in range(0, n)]
-    
-    
+    primes = [True for i in range(0, n + 1)]
+    m = math.floor(math.sqrt(n))
+    count = 2
+    while(count <= m):
+        if primes[count]:
+            m_in = n/count
+            for i in range(2 * count, m_in * count + 1, count):
+                primes[i] = False
+        count = count + 1
+    return primes
+count = 0
+print "prime Numbers"
+for i in get_primes(28):
+    if i: sys.stdout.write(str(count) + " ")
+    count = count + 1
+print "\nend" 
