87ed6978ea43bbfe67a1b2aa35b026abf1e7488d
anupamsharma <anu.lnmiit.1985@gmail.com>
2013-11-28 11:38:44 +0530

    Update algo_util.py
    Adding method gcd to calculate greatest common divisor using Euclid's method.

--- algo_util.py
+++ algo_util.py
@@ -5,6 +5,9 @@ def get_primes(n):
     """
     Returns a n+1 length boolean list a indicating if i is prime by value of a[i]. It usage Sieve of Eratosthenes for
     this.
+    doctest
+    >>> get_primes(5)
+    [True, True, True, True, False, True]
 
     """
     assert(n>1)
@@ -24,3 +27,36 @@ for i in get_primes(28):
     if i: sys.stdout.write(str(count) + " ")
     count = count + 1
 print "\nend" 
+
+def gcd(a, b):
+    """
+    Calculates Greatest common devisor of two number using Euclid's theorem.
+    >>> gcd(5,4)
+    1
+    
+    >>> gcd(10,4)
+    2
+    
+    >>> gcd(50,10)
+    10
+    
+    >>> gcd(1, 20)
+    1
+    
+    >>> gcd(20, 20)
+    20
+    
+    """
+    if (a==b):
+        return a
+        
+    if (a==1 or b==1):
+        return 1
+    
+    x1 = max(a,b)
+    x2 = min(a,b)
+    rem = x1 % x2
+    if rem == 0:
+        return x2
+    else:
+        return gcd(x2, rem)
